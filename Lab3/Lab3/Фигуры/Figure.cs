f6d267255172899dae162d5805bd54a1cdd02220
Ivan Bondarenko <ivan.frinom@gmail.com>
2020-09-07 23:11:16 +0300

    Lab3 done

---
+++ Lab3/Lab3/Фигуры/Figure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab3
+{
+    abstract class Figure : IComparable, IPrint
+    {
+        protected string Type;
+        public abstract double getArea();
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+        public int CompareTo(object obj)
+        {
+            Figure robj = obj as Figure;
+            if (robj != null)
+            {
+                return this.getArea().CompareTo(robj.getArea());
+            }
+            else
+                throw new Exception("Несравнимые объекты");
+        }
+        public override string ToString()
+        {
+            return $"{this.Type} площадью {this.getArea()}";
+        }
+    }
+}
---
+++ Lab3/Lab3/Фигуры/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab3
+{
+    class Rectangle : Figure
+    {
+        protected double length, width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+            Type = "Прямоугольник";
+        }
+        public override double getArea()
+        {
+            return length * width;
+        }
+    }
+}
