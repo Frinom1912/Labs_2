ca5028f8a714b3a7e9cc30f38584b9be66459019
Ivan Bondarenko <ivan.frinom@gmail.com>
2020-09-14 00:09:47 +0300

    Lab6 done

---
+++ Lab6/Lab6.2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6._2
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false,
+        Inherited = false)]
+    public class MyAttribute : System.Attribute
+    {
+        public string Description { get; set; }
+        public MyAttribute()
+        {
+
+        }
+        public MyAttribute(string Description)
+        {
+            this.Description = Description;
+        }
+    }
+    public class MyString
+    {
+        [MyAttribute(Description = "Атрибут MyAttribute: data")]
+        public char[] data { get { return _data; } set { data = null; } }
+        public int length { get; private set; }
+        private char[] _data;
+        public MyString()
+        {
+            data = null;
+            length = 0;
+        }
+
+        public MyString(char[] str)
+        {
+            length = str.Length;
+            data = new char[length];
+            int i = 0;
+            foreach (char a in str)
+            {
+                data[i] = a;
+                i++;
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < length; i++)
+                Console.Write(data[i]);
+        }
+        public int getLength() { return length; }
+    }
+
+    class Program
+    {
+        public static bool GetAttributeProperty(PropertyInfo propertyInfo, Type type, out object att)
+        {
+            bool res = false;
+            att = null;
+            var isAtt = propertyInfo.GetCustomAttributes(type, false);
+            if (isAtt.Length > 0)
+            {
+                res = true;
+                att = isAtt[0];
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\tРефлексия");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Type type = Type.GetType("Lab6._2.MyString");
+
+            MethodInfo[] methods = type.GetMethods();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Вывод методов:");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine("\t" + method.Name);
+            }
+
+            PropertyInfo[] props = type.GetProperties();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nВывод свойств:");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            foreach (PropertyInfo prop in props)
+            {
+                Console.WriteLine("\t" + prop.PropertyType.ToString() + " " + prop.Name);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nВывод свойств с атрибутом:");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            foreach (var x in props) 
+            { 
+                object attrObj; 
+                if (GetAttributeProperty(x, typeof(MyAttribute), out attrObj)) 
+                { 
+                    MyAttribute attr = attrObj as MyAttribute; 
+                    Console.WriteLine(x.Name + " - " + attr.Description); 
+                } 
+            }
+
+        }
+    }
+}
--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -8,7 +8,7 @@ namespace Lab6
 {
     class Program
     {
-        delegate int delegateEx(int a, int b);
+        public delegate int delegateEx(int a, int b);
 
         public static int Sum(int a, int b)
         {
@@ -18,11 +18,53 @@ namespace Lab6
         {
             return a - b; 
         }
+
+        public static int SimpleCalc(int a, int b, delegateEx deg)
+        {
+            return deg.Invoke(a, b);
+        }
+
+        public static int SimpleCalcFunc(int a, int b, Func<int,int,int> deg)
+        {
+            return deg.Invoke(a, b);
+        }
+
         static void Main(string[] args)
         {
-            delegateEx obj =  Sum;
-            Console.WriteLine(obj.Invoke(4, 5));
+            int a, b;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\tДелегаты");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            while (true)
+            {
+                Console.Write("Введите число №1: ");
+                string line = Console.ReadLine();
+                if (!Int32.TryParse(line, out a))
+                {
+                    Console.WriteLine("Ошибка ввода!");
+                }
+                else
+                    break;
+            }
+            while (true)
+            {
+                Console.Write("Введите число №2: ");
+                string line = Console.ReadLine();
+                if (!Int32.TryParse(line, out b))
+                {
+                    Console.WriteLine("Ошибка ввода!");
+                }
+                else
+                    break;
+            }
 
+            Console.Write($"Функция в качестве аргумента: {a} + {b} = ");
+            Console.WriteLine(SimpleCalc(a, b, Sum));
+            Console.Write($"Лямбда-выражение в качестве аргумента: {a} + {b} = ");
+            Console.WriteLine(SimpleCalc(a, b, (m, n) => (m + n)));
+            Func<int, int, int> res = Sum;
+            Console.Write($"Func<> в качестве аргумента: {a} + {b} = ");
+            Console.WriteLine(SimpleCalcFunc(a, b, res));
         }
     }
 }
